613a9d1cf7329d10662bfec27862b10965de359d
Dtheresa <derryntheresa@gmail.com>
2020-08-21 18:28:05 +0530

    music - views, urls, templates modified

--- music/views.py
+++ music/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Album
 
 
 # Create your views here.
 def song_list(request):
-    return render(request, 'music/song_list.html', {})
+	country_songs = Album.objects.filter(genre__lte='Country')
+	kpop_songs = Album.objects.filter(genre='K-pop')
+	pop_songs =  Album.objects.filter(genre='Pop')
+	rap_songs = Album.objects.filter(genre='Rap')
+	return render(request, 'music/song_list.html', {'country_songs': country_songs, 'kpop_songs': kpop_songs, 'pop_songs': pop_songs, 'rap_songs': rap_songs})
